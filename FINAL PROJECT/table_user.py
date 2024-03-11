bec83e0816bb3d462ae7dab51e84469ead906c8a
Maksim Kocheshkov <max.kocheshkov@gmail.com>
2024-03-11 23:57:25 +0100

    Create table_user.py

---
+++ FINAL PROJECT/table_user.py
@@ -0,0 +1,13 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    gender = Column(Integer)
+    age = Column(Integer)
+    country = Column(String)
+    city = Column(String)
+    exp_group = Column(Integer)
+    os = Column(String)
+    source = Column(String)
