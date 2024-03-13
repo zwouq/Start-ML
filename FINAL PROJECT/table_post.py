5029e559ba7548be98b132a4b24dcd0540fb53b2
Maksim Kocheshkov <max.kocheshkov@gmail.com>
2024-03-13 03:09:10 +0100

    Update table_post.py

--- FINAL PROJECT/table_post.py
+++ FINAL PROJECT/table_post.py
@@ -1,8 +1,11 @@
 from database import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from table_feed import Feed
 
 class Post(Base):
     __tablename__ = "post"
     id = Column(Integer, primary_key=True)
     text = Column(String)
     topic = Column(String)
+    feeds = relationship(Feed)
