a4dc8e429a78d11638ef46868862af0d01381084
Maksim Kocheshkov <max.kocheshkov@gmail.com>
2024-03-13 03:09:23 +0100

    Update 2. simple_select.py

--- FINAL PROJECT/2. simple_select.py
+++ FINAL PROJECT/2. simple_select.py
@@ -14,5 +14,3 @@ if __name__ == "__main__":
     business_posts = session.query(Post.id).filter(Post.topic == "business").order_by(Post.id.desc()).limit(10).all()
     business_post_ids = [post.id for post in business_posts]
     print(business_post_ids)
-
-
