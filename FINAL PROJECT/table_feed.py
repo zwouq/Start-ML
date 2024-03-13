e47fa2e9b19d9a30473afffa7ae4c8ecc3cf3fd3
Maksim Kocheshkov <max.kocheshkov@gmail.com>
2024-03-13 03:09:12 +0100

    Update table_feed.py

--- FINAL PROJECT/table_feed.py
+++ FINAL PROJECT/table_feed.py
@@ -2,11 +2,15 @@ from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
 from database import Base
 from table_user import User
 from table_post import Post
+from sqlalchemy.orm import relationship
 
 class Feed(Base):
     __tablename__ = "feed_action"
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
     post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
     action = Column(String)
     time = Column(DateTime)
+    
